ceda7be63ed364bc4f3cdeb6e07c276a2c011d75
Matv <syusyukin@nlmk.com>
2024-09-22 13:39:16 +0400

    add platforms

--- src/entities/platform.py
+++ src/entities/platform.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import base_platform
 
+
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         super().__init__()
--- src/settings.py
+++ src/settings.py
@@ -2,24 +2,18 @@ from typing import Tuple
 from pydantic import BaseModel
 
 
-class ColorRGB(BaseModel):
-    color: Tuple[int, int, int]
-
-
 class Platform(BaseModel):
-    image_fill: ColorRGB
+    image_fill: Tuple[int, int, int]
 
 
-class ScreenParams(BaseModel):
-    main_window_size: Tuple[int, int]
-    background_color: ColorRGB
-    screen_caption: str
+class ScreenParams:
+    MAIN_WINDOW_SIZE: Tuple[int, int] = (600, 400)
+    BACKGROUND_COLOR: Tuple[int, int, int] = (125, 30, 255)
+    SCREEN_CAPTION: str = "The Game"
 
+class UserSettings(BaseModel):
+    FPS: int
 
-main_screen_params = ScreenParams(
-    main_window_size=(300, 300),
-    background_color=ColorRGB(color=(125, 30, 255)),
-    screen_caption="The game",
-)
+base_platform = Platform(image_fill=(255, 255, 255))
 
-base_platform = Platform(image_fill=ColorRGB(color=(255, 255, 255)))
+user_settings = UserSettings(FPS=60)
